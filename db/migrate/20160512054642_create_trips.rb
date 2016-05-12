697172bc18593c251d3c466b6a1f7c70c6126ae3
byronbenjamin <byrongage@yahoo.com>
2016-05-12 11:39:54 -0500

    add migrations and paperclip

---
+++ db/migrate/20160512054642_create_trips.rb
@@ -0,0 +1,14 @@
+class CreateTrips < ActiveRecord::Migration
+  def change
+    create_table :trips do |t|
+      t.integer :user_id
+      t.string :location
+      t.string :month
+      t.string :year
+      t.string :description
+      t.string :method_of_travel
+
+      t.timestamps null: false
+    end
+  end
+end
