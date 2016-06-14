67ea016395be7eba3a89eb21d8b4d4dd5e18fc74
byronbenjamin <byrongage@yahoo.com>
2016-06-14 00:53:07 -0500

    add sorted trips visited list to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @trips = @user.trips.all.to_json
+    @trips = @user.trips.order("trips.name").all
   end
 
   # GET /users/new
